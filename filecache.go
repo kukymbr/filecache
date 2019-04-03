53537c637ecebc7a1b2c9096df8c4156a118c1ce
Sergey Basov <sergey@espressio.ru>
2019-04-04 02:16:19 +0300

    [godoc] package comment - change word

--- filecache.go
+++ filecache.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-// Package filecache is a package to write data from any io.Reader
+// Package filecache is a tool to write data from any io.Reader
 // to cache files with TTL and metadata.
 package filecache
 
